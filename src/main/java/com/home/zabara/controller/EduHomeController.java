9e68a65c22323aaaa37d1cec75dc96ad2710b305
Pavlo_Zabara <zabarapasha09@gmail.com>
2021-02-25 15:43:51 +0200

    add property

--- src/main/java/com/home/zabara/controller/EduHomeController.java
+++ src/main/java/com/home/zabara/controller/EduHomeController.java
@@ -1,14 +1,21 @@
 package com.home.zabara.controller;
 
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EduHomeController {
+    @Value("${userNameForGreeting}")
+    private String userNameForGreeting;
 
     @GetMapping("/hello")
     public ResponseEntity<String> helloThere() {
-        return ResponseEntity.ok("Hello there, this is sample app");
+        String greeting = StringUtils.isEmpty(userNameForGreeting)
+                ? "Hello there, this is sample app"
+                : "Hello " + userNameForGreeting + ", have a nice day";
+        return ResponseEntity.ok(greeting);
     }
 }
